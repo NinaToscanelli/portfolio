067a3a71af1cf856df3a49661a99e5cad7ad43c4
ninatoscanelli <nina.toscanelli@studentfr.ch>
2025-05-07 12:00:25 +0200

    effet nuit

--- src/js/main.js
+++ src/js/main.js
@@ -12,15 +12,24 @@ const swiper = new Swiper(".swiper-container", {
 
 var day = document.querySelector(".mode-day");
 var night = document.querySelector(".mode-night");
+var mode = sessionStorage.getItem("mode");
+
+if (mode == "night") {
+  night.classList.remove("is-active");
+  day.classList.add("is-active");
+  document.body.classList.add("night");
+}
 
 night.addEventListener("click", function () {
   night.classList.remove("is-active");
   day.classList.add("is-active");
   document.body.classList.add("night");
+  sessionStorage.setItem("mode", "night");
 });
 
 day.addEventListener("click", function () {
   day.classList.remove("is-active");
   night.classList.add("is-active");
   document.body.classList.remove("night");
+  sessionStorage.setItem("mode", "day");
 });
