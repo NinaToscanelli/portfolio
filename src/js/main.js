fdfddcc5b2f4d8f1a78eecd70aca8e7aa4dae7f8
ninatoscanelli <nina.toscanelli@studentfr.ch>
2025-12-18 16:59:17 +0100

    final responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,43 @@
 import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
 
-Swiper.use([Navigation, Pagination, EffectFade]);
+document.addEventListener("DOMContentLoaded", function () {
+  Swiper.use([Navigation, Pagination, EffectFade]);
 
-const swiper = new Swiper(".swiper-container", {
-  effect: "fade",
-  navigation: {
-    nextEl: ".arrow-right",
-    prevEl: ".arrow-left",
-  },
-});
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    effect: "fade",
+    navigation: {
+      nextEl: ".arrow-right",
+      prevEl: ".arrow-left",
+    },
+  });
 
-var day = document.querySelector(".mode-day");
-var night = document.querySelector(".mode-night");
-var mode = sessionStorage.getItem("mode");
+  var day = document.querySelector(".mode-day");
+  var night = document.querySelector(".mode-night");
+  var mode = sessionStorage.getItem("mode");
 
-if (mode == "night") {
-  if (night) {
-    night.classList.remove("is-active");
-    day.classList.add("is-active");
+  if (mode == "night") {
+    if (night) {
+      night.classList.remove("is-active");
+      day.classList.add("is-active");
+    }
+
+    document.body.classList.add("night");
   }
 
-  document.body.classList.add("night");
-}
+  if (night) {
+    night.addEventListener("click", function () {
+      night.classList.remove("is-active");
+      day.classList.add("is-active");
+      document.body.classList.add("night");
+      sessionStorage.setItem("mode", "night");
+    });
 
-if (night) {
-  night.addEventListener("click", function () {
-    night.classList.remove("is-active");
-    day.classList.add("is-active");
-    document.body.classList.add("night");
-    sessionStorage.setItem("mode", "night");
-  });
-
-  day.addEventListener("click", function () {
-    day.classList.remove("is-active");
-    night.classList.add("is-active");
-    document.body.classList.remove("night");
-    sessionStorage.setItem("mode", "day");
-  });
-}
+    day.addEventListener("click", function () {
+      day.classList.remove("is-active");
+      night.classList.add("is-active");
+      document.body.classList.remove("night");
+      sessionStorage.setItem("mode", "day");
+    });
+  }
+});
